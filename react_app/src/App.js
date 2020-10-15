68faa5c381d73f966fbb8b6722e2baaae6182777
suguru-3u <suguru.13579515@gmail.com>
2020-10-15 10:31:53 +0900

    Creat Rect.js

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -2,11 +2,42 @@ import React,{Component} from 'react';
 import './App.css';
 
 class App extends Component{
+
   render(){
     return<div>
       <h1>React</h1>
-      <p>This is sample component</p>
+      <Rect x = "50" y = "50" w = "150" h="150" c ="cyan" /> 
       </div>;
+  }
+}
+
+class Rect extends Component{
+  x = 0;
+  y = 0;
+  width = 0;
+  height = 0;
+  color = "white";
+  style = {};
+
+  constructor(props){
+    super(props);
+    this.x = props.x;
+    this.y = props.y;
+    this.width = props.w;
+    this.height = props.h;
+    this.color = props.c;
+    this.style = {
+      backgroundColor:this.color,
+      position:"absolute",
+      left:this.x + "px",
+      top:this.y + "px",
+      width:this.width + "px",
+      height:this.height + "px"
+    }
+  }
+
+  render(){
+    return <div style={this.style}></div>;
   }
 }
 
