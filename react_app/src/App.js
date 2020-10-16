dcdf5e35d0d0af2f24048679599fb6f145d3b721
suguru-3u <suguru.13579515@gmail.com>
2020-10-16 09:59:33 +0900

    Create form

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -7,10 +7,15 @@ class App extends Component{
   input = '';
 
   msgStyle = {
-    fontSize:"24pt",
+    fontSize:"20pt",
     color:"#900",
     margin:"20px 0px",
     padding:"5px",
+  }
+
+  inputStyle = {
+    fontSize:"12pt",
+    padding:"5px"
   }
 
   constructor(props){
@@ -36,38 +41,44 @@ class App extends Component{
   render(){
     return<div>
         <h1>React</h1>
-        <Message title="Children!">
-          これはコンポーネント内のコンテンツです。
-        </Message>
+        <h2>{this.state.message}</h2>
+        <form onSubmit={this.doSubmit}>
+          <label>
+            <span style={this.inputStyle}></span>Message;
+            <input type="text" style={this.inputStyle} onChange={this.doChange} 
+            required pattern="[A-Za-z_,.]+"/>
+          </label>
+          <input type="submit" style={this.inputStyle} value="Click"></input>
+        </form>
       </div>;
   }
 }
 
-class Message extends Component{
-  li = {
-    fontSize:"16pt",
-    color:"#06",
-    margin:"0px",
-    padding:"0px"
-  }
+// class Message extends Component{
+//   li = {
+//     fontSize:"16pt",
+//     color:"#06",
+//     margin:"0px",
+//     padding:"0px"
+//   }
 
-  render(){
-    let content = this.props.children;
-    let arr = content.split('。');
-    let arr2 = [];
-    for(let i = 0; i < arr.length; i++){
-      if(arr[i].trim() != ''){
-        arr2.push(arr[i]);
-      }
-    }
-    let list = arr2.map((value,key) => (
-    <li style={this.li} key={key}>{value}.</li> )
-    );
-    return <div>
-      <h2>{this.props.title}</h2>
-      <ol>{list}</ol>
-    </div>
-  }
-}
+//   render(){
+//     let content = this.props.children;
+//     let arr = content.split('。');
+//     let arr2 = [];
+//     for(let i = 0; i < arr.length; i++){
+//       if(arr[i].trim() != ''){
+//         arr2.push(arr[i]);
+//       }
+//     }
+//     let list = arr2.map((value,key) => (
+//     <li style={this.li} key={key}>{value}.</li> )
+//     );
+//     return <div>
+//       <h2>{this.props.title}</h2>
+//       <ol>{list}</ol>
+//     </div>
+//   }
+// }
 
 export default App;
