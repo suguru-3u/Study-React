e349b9b662ffddb08bc92bd3f47ceb1808ee45fa
suguru-3u <suguru.13579515@gmail.com>
2020-10-18 15:55:43 +0900

    create component file

--- next_app/pages/index.js
+++ next_app/pages/index.js
@@ -1,34 +1,14 @@
-const h1 = {
-    fontSize:'72pt',
-    fontWeight:'bold',
-    textAlign:'right',
-    letterSpacing:'-8px',
-    color:'#0f0f0',
-    margin:'-40px 0px'
-}
-
-const p = {
-    fontSize:'16pt',
-    color:'#666',
-    margin:'0px'
-}
+import Link from 'next/link'
+import style from '../static/Style'
 
 export default () => <div>
-    <style jsx>{`
-         h1{
-            font-size:68pt;
-            font-weight:bold;
-            text-align:right;
-            letter-spacing:-8px;
-            color:#0f0f0;
-            margin:-32px 0px;
-        }
-        p{
-            font-size:16pt;
-            color:#666;
-            margin:0px;
-        }
-    `}</style>
+    {style}
     <h1>Next.js</h1>
     <p>Welcome to next</p>
+    <hr />
+    <div>
+        <Link href="/other">
+            <a>Go to Other</a>
+        </Link>
+    </div>
 </div>
---
+++ next_app/static/Style.js
@@ -0,0 +1,18 @@
+export default <style>{`
+    body{
+        margin:10px;
+    }
+    h1{
+        fontSize:'72pt',
+        fontWeight:'bold',
+        textAlign:'right',
+        letterSpacing:'-8px',
+        color:'#0f0f0',
+        margin:'-40px 0px'
+    }
+    p{
+        fontSize:'16pt',
+        color:'#666',
+        margin:'0px'
+    }
+`}</style>;
